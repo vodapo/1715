7035451f73ba0d38a5243a12f8c4f839f153d30c
vodapo <34284848+vodapo@users.noreply.github.com>
2018-12-10 16:09:40 +0100

    Add files via upload

---
+++ 07TimesTable.py
@@ -0,0 +1,27 @@
+def print_list(items, header=None):
+    if header != None:
+        print(header)
+    for item in items:
+        print(str(item))
+    print()
+
+
+def even(items):
+    return items[::2]
+
+
+def odd(items):
+    return items[1::2]
+
+
+times_table = int(input("Which times table please!!!!!!!!? "))
+
+table = []
+for counter in range(13):
+    table.append(times_table * (counter))
+
+print_list(even(table), "The " + str(times_table) + " times table - even values")
+print_list(odd(table), "The " + str(times_table) + " times table - odd values")
+
+print()
+input("Press return to continue ...")
